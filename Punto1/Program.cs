79fe5d23586e376c2fdc35fa604f78b649ff9c90
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-04-05 20:37:17 -0500

    Actualizacion commit

---
+++ Punto1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Punto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Juego adivina El numero 
+            //Muestra el mensaje de entrada
+            Console.WriteLine("Bienvenidos al juego Adivina el numero o quedaras atrapado");
+
+            // Creacion de Bucle para los jugadores 
+
+            while (true);
+            {
+
+                //Declaramos las variables 
+                int numJugadores, numMin = 0 , numMax = 0;
+
+                // numero de jugadores 
+
+                Console.WriteLine("Ingrese el numero de Jugadores (Entre 2 y 4 Jugadores): "); 
+                numJugadores = Convert.ToInt32(Console.ReadLine());
+
+                switch (numJugadores)
+                {
+                    case 2:
+                        numMax = 50;
+                        break;
+                    case 3:
+                        numMax = 100;
+                        break;
+                    case 4:
+                        numMax = 200;
+                        break;
+                    default:
+                        Console.WriteLine("No es posible agregar mas Jugadores");
+                    
+                }
+                int rango = new Random().Next(numMin, mi)
+
+            }   
+
+        }
+    }
+}
